1de8dd332ad86981e3ba7e464be9459cbc0bdf34
Mizok <karasunohina@gmail.com>
2021-07-16 00:09:24 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+export * from './lib/base';
+export * from './lib/function';
+export * from './lib/shape';
+export * from './lib/entity';
+export * from './lib/interpolation';
+export * from './lib/noise';
+export * from './lib/blur';
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,14 @@
 const { resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: ['./src/js/index.js', './src/scss/main.scss'],
+    main: ['./index.js'],
   },
   output: {
     filename: 'assets/js/[name].js',
     chunkFilename: '[name].min.js',
     path: resolve(__dirname, 'build'),
     clean: true,
-  },
-  devServer: {
-    contentBase: resolve(__dirname, 'build'),
-    open: true,
-    compress: true,
   },
   mode: 'development',
   module: {
@@ -32,70 +24,16 @@ module.exports = {
             }
           }
         ],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          }
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/img/[name][ext]'
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '../../'
-            }
-          },
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                ident: 'postcss',
-                plugins: [
-                  require('postcss-preset-env')()
-                ]
-              }
-            }
-          },
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(otf|eot|ttf|woff2?)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/font/[name][ext]'
-        }
       }
-
     ]
   },
   resolve: {
     alias: {
-      '@img': resolve(__dirname, './src/img'),
-      '@font': resolve(__dirname, './src/font')
+      // '@img': resolve(__dirname, './src/img'),
+      // '@font': resolve(__dirname, './src/font')
     }
   },
   plugins: [
-    new OptimizeCssAssetsWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: './assets/css/[name].css'
-    }),
-    new HtmlWebpackPlugin({
-      filename: `index.html`,
-      template: './index.html'
-    })
+
   ]
 }
